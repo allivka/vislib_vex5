dff98216eab7db248f4fb9f1793dd54b04b39810
allivka <allivka1920@gmail.com>
2025-09-28 15:35:40 +0300

    did some stuff

--- include/base.hpp
+++ include/base.hpp
@@ -9,18 +9,31 @@ using AnglePosition = int;
 using DistancePosition = int;
 using Speed = int;
 
+class SpeedRange {
+public:
+    Speed lowest = 0;
+    Speed highest = 0;
+    
+    SpeedRange() = default;
+    SpeedRange(Speed p_lowest, Speed p_highest) : lowest(p_lowest), highest(p_highest) {}
+};
+
 class MotorInfo {
 public:
-    AnglePosition angle;
-    DistancePosition distance;
-    Speed lowSpeed;
-    Speed highSpeed;
+    AnglePosition angle = 0;
+    DistancePosition distance = 0;
+    SpeedRange speedRange;
     
-    MotorInfo(AnglePosition p_a, DistancePosition p_d, Speed lowLimit, Speed highLimit) : angle(p_a), distance(p_d), lowSpeed(lowLimit), highSpeed(highLimit) {}
+    MotorInfo(AnglePosition p_a, DistancePosition p_d, SpeedRange p_speed) : angle(p_a), distance(p_d), speedRange(p_speed) {}
     
-    Speed FitSpeed2Range(Speed speed, Speed lowestSpeed, Speed highestSpeed) {
-        return arduino::map(speed, lowestSpeed, highestSpeed, lowSpeed, highSpeed);
+    Speed FitSpeed2Range(Speed speed, SpeedRange range) {
+        return arduino::map(speed, range.lowest, range.highest, speedRange.lowest, speedRange.highest);
     }
+};
+
+class MotorController {
+public:
+    virtual void setSpeed() = 0;
 };
 
 }
